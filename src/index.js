06c87d54ab03715ff2a9aa53fd7b568f5d31db50
pgu <philipp@matchwerk.de>
2019-01-09 16:52:51 +0100

    listen to start and stop events

--- src/index.js
+++ src/index.js
@@ -9,6 +9,10 @@ const config = {
     log_dir: './logs'
 }
 
+const state = {
+    streams: {}
+}
+
 class DockerLogTransform extends Transform {
     _transform(chunk, encoding, callback) {
         callback(null, chunk.slice(8))
@@ -17,8 +21,8 @@ class DockerLogTransform extends Transform {
 
 const pipeLog = async (container) => {
     const id = container.Id
-    const name = container.Names[0]
-    const log_filename = `${config.log_dir}${name}.log`
+    const name = container.Names[0].replace('/', '')
+    const log_filename = `${config.log_dir}/${name}.log`
     const log_out_stream = fs.createWriteStream(log_filename, {
         flags: 'a',
         encoding: 'utf8'
@@ -38,13 +42,28 @@ const pipeLog = async (container) => {
     console.log(`streaming logs for ${name} (${id}) to ${log_filename}`)
 
     log_in_stream.pipe(new DockerLogTransform()).pipe(log_out_stream)
-    
-    setTimeout(() => {
+
+    state.streams[id] = { log_in_stream, log_out_stream }
+}
+
+const handleDockerEvent = data => {
+    const event = JSON.parse(data.toString())
+    const { Action, Actor: { ID }, Type } = event
+
+    if(Action === 'stop' && Type === 'container' && state.streams[ID]){
+        const { log_in_stream, log_out_stream } = state.streams[ID]
+
+        console.log(`received stop event for ${ID}, closing streams`)
+
         log_in_stream.destroy()
         log_out_stream.destroy()
-    }, 5000)
+        delete state.streams[ID]
+    }
+    else if(Action === 'start' && Type === 'container'){
+        console.log(`received start event for ${ID}`)
 
-    return {container, log_in_stream, log_out_stream}
+        pipeLog({ Id: ID, Names: [event.Actor.Attributes.name] })
+    }
 }
 
 const main = async () => {
@@ -53,6 +72,13 @@ const main = async () => {
         console.log(`${ps_res.data.length} containers running`)
 
         ps_res.data.forEach(pipeLog)
+
+        const event_res = await client.get(`/events`, {
+            responseEncoding: 'utf8',
+            responseType: 'stream'
+        })
+        const event_stream = event_res.data
+        event_stream.on('data', handleDockerEvent )
     }
     catch(err) {
         console.log(util.inspect(err))
